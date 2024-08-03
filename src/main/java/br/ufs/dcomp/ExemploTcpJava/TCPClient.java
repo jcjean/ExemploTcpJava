33a248dd09d9aaa71710ce07f570b16b6f38c307
Ubuntu <ubuntu@ip-172-31-89-91.ec2.internal>
2024-08-03 05:11:56 +0000

    troca de mensagens no tcpserver e cliente

--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
@@ -4,22 +4,40 @@
 package br.ufs.dcomp.ExemploTcpJava;
 
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
 public class TCPClient{
     public static void main(String[] args){
         try {
+            Scanner scan = new Scanner(System.in);
             System.out.print("[ Conectando com o Servidor TCP    ..................  ");
             Socket sock = new Socket("127.0.0.1", 3300);
             System.out.println("[OK] ]");
             
             InputStream is = sock.getInputStream(); // Canal de entrada de dados
             OutputStream os = sock.getOutputStream(); // Canal de saída de dados
-            String msg = "Olá, DCOMP!!!";
-            byte[] buf = msg.getBytes(); // Obtendo a respresntação em bytes da mensagem
+            int count=0;
 
-            System.out.print("[ Enviando mensagem    ..............................  ");
-            os.write(buf);
-            System.out.println("[OK] ]");
+            do{
+                String msg = scan.nextLine();
+                
+                if(msg.trim().equals("sair")){
+                    byte[] buf = msg.getBytes(); // Obtendo a respresntação em bytes da mensagem
+                    os.write(buf);
+                    System.out.println("[OK]");
+                    count++;
+                }else{
+                    byte[] buf = msg.getBytes(); // Obtendo a respresntação em bytes da mensagem
+                    os.write(buf);
+                    System.out.println("[OK]");
+                    
+                    byte[] buf2 = new byte[20];
+                    is.read(buf2);
+                    String resp = new String(buf2);
+                    System.out.println("  ACK>> "+ resp);
+                }
+            }while(count==0);
+            
         }catch(Exception e){System.out.println(e);}    
         System.out.println("[ FIM ]");
     }
--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPServer.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPServer.java
@@ -4,11 +4,13 @@
 package br.ufs.dcomp.ExemploTcpJava;
 
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
 public class TCPServer{
     public static void main(String[] args){
         
         try {
+            Scanner scan = new Scanner(System.in);
             System.out.print("[ Iniciando Servidor TCP    .........................  ");
             ServerSocket ss = new ServerSocket(3300, 5, InetAddress.getByName("127.0.0.1"));
             System.out.println("[OK] ]");
@@ -20,14 +22,26 @@ public class TCPServer{
             InputStream is = sock.getInputStream(); //Canal de entrada de dados
             OutputStream os = sock.getOutputStream(); //Canal de saída de dados
             byte[] buf = new byte[20]; // buffer de recepção
-
-            System.out.print("[ Aguardando recebimento de mensagem   ..............  ");
-            is.read(buf); // Operação bloqueante (aguardando chegada de dados)
-            System.out.println("[OK] ]");
+            //String aux = "0";
+            int count=0;
             
-            String msg = new String(buf); // Mapeando vetor de bytes recebido para String
+            System.out.print("[ .............. ]");
+            do{
+                is.read(buf); // Operação bloqueante (aguardando chegada de dados)
             
-            System.out.println("  Mensagem recebida: "+ msg);
+                String msg = new String(buf); // Mapeando vetor de bytes recebido para String
+                System.out.println("[OK] ]");
+                System.out.println("  Mensagem recebida: "+ msg);
+                
+                if(msg.equals("sair")){
+                    System.out.println("  Conversa encerrada>> "+ msg);
+                    count++;
+                }else{
+                    String ack = scan.nextLine();
+                    byte[] buf2 = ack.getBytes();
+                    os.write(buf2);
+                }
+            }while(count==0);
         }catch(Exception e){System.out.println(e);}    
         System.out.println("[ FIM ]");
     }
